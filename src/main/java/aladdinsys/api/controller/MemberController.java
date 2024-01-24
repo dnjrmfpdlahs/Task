31ec037f17fc61bb62037e435bbe47467e841c5f
dnjrmfpdlahs <zhsks9709@aladdinsys.co.kr>
2024-01-24 16:32:58 +0900

    스웨거 적용

--- src/main/java/aladdinsys/api/controller/MemberController.java
+++ src/main/java/aladdinsys/api/controller/MemberController.java
@@ -3,6 +3,7 @@ package aladdinsys.api.controller;
 import aladdinsys.api.dto.LoginDTO;
 import aladdinsys.api.dto.MemberDTO;
 import aladdinsys.api.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +16,12 @@ public class MemberController {
     private MemberService memberService;
 
 
+    @Operation(summary = "회원 가입 요청", description = "회원가입 진행.")
     @PostMapping("/signup")
     public void signUp(MemberDTO dto) throws Exception {
         memberService.signUp(dto);
     }
-
+    @Operation(summary = "로그인 요청", description = "로그인 진행.")
     @PostMapping("/login")
     public ResponseEntity<String> login(
             @RequestBody LoginDTO dto) {
@@ -30,7 +32,7 @@ public class MemberController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
         }
     }
-
+    @Operation(summary = "정보확인 요청", description = "정보확인 진행.")
     @GetMapping("/me")
     public ResponseEntity<MemberDTO> getMyInfo(@RequestHeader("Authorization") String jwtToken) {
         if (jwtToken == null || !jwtToken.startsWith("Bearer ")) {
--- src/main/java/aladdinsys/api/utils/SecurityConfig.java
+++ src/main/java/aladdinsys/api/utils/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
